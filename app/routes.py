267563ab83a52715473cd7f5ba03fb25d381969c
EC2 Default User <ec2-user@ip-172-31-22-175.us-east-2.compute.internal>
2019-07-24 17:25:31 +0000

    tribute v1

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,17 @@ from app.models import model, formopener
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello world!"
+    return render_template("index.html")
+
+@app.route('/psudeonym')
+def psudeonym():
+    return render_template("psudeonym.html")
+    
+@app.route('/thanksSub', methods = ["GET", "POST"])
+def thanksSub():
+    if request.method == 'GET':
+        return "Login Now."
+    else:
+        userData = dict(request.form)
+        userName = userData['userName']
+        return render_template("thanksSub.html", userName = userName)
